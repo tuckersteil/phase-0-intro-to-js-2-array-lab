e3343203f202ad315699e8bab7ebafbed932627a
Tucker Steil <tuckersteil@gmail.com>
2022-03-29 17:59:29 -0400

    commit tree c2e28e4ec6a760b5254bf2521407013473f87006

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+   appendCat =  [...cats, name]
+   return (appendCat)
+}
+
+function prependCat(name){
+    return prependCat = [name, ...cats]
+}
+
+function removeLastCat(){
+    return cats.slice(0, 2)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
